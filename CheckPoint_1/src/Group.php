10944bf8ba509c4faa18ae2e05858017fecd3441
andela-gjames <george.james@andela.com>
2015-12-03 23:55:04 +0100

    Added and Edited some Comments

--- CheckPoint_1/src/Group.php
+++ CheckPoint_1/src/Group.php
@@ -1,6 +1,14 @@
 <?php
+/**
+ * Class for Grouping Strings
+ **/
   class Group
   {
+    /**
+     * @param $string the string to count occurance of values
+     * @throws InvalidArgumentException if the argument passed is not a string or if it is null
+     * @return Array of the ranked words from string
+     */
     public static function build($string=null)
     {
       //Ensure Correct values is passed into function
@@ -9,7 +17,7 @@
       elseif(is_int($string))
         throw new InvalidArgumentException("This function only accepts strings");
 
-      // Removes all AlphaNumeric Characters from passed arguments
+      // Removes all non AlphaNumeric Characters from passed arguments
       $string = preg_replace("/[^A-Za-z0-9\s]/", '', $string);
       //Converts String into array using space as delimiter
       $string_array = array_filter(explode(" ",$string));
